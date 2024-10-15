f34a2e443e4d223bbe1bf01deb9c27e943a8e92f
Azona Isagba <isagbaazona@gmail.com>
2024-10-14 20:37:12 -0700

    Implement navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormControl,
+    FormGroup
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'Arch CRM';
   name = 'Anna';
-}
+
+ /* clientUserProfile = new FormGroup ({
+    firstName: new FormControl (''),
+    lastName : new FormControl(''),
+    email : new FormControl(''),
+    address : new FormControl('')
+
+  });
+
+  onSubmit() {
+    console.log(this.clientUserProfile.value)
+  } */
+} 
