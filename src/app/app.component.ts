ca1d5461bd14215021108f149aa17defc23c8bab
Azona Isagba <isagbaazona@gmail.com>
2024-10-14 20:56:57 -0700

    implement changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormGroup, FormControl } from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , FormControl,
-    FormGroup
+  imports: [RouterOutlet 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -16,15 +15,5 @@ export class AppComponent {
   title = 'Arch CRM';
   name = 'Anna';
 
- /* clientUserProfile = new FormGroup ({
-    firstName: new FormControl (''),
-    lastName : new FormControl(''),
-    email : new FormControl(''),
-    address : new FormControl('')
-
-  });
-
-  onSubmit() {
-    console.log(this.clientUserProfile.value)
-  } */
+ 
 } 
